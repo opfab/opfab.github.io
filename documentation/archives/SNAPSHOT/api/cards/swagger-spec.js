133aca21193258f0cd6574e81aa69499d1b81a6c
OpfabTech <opfabtech@gmail.com>
2024-04-06 00:59:24 +0200

    Updating documentation for version SNAPSHOT

--- documentation/archives/SNAPSHOT/api/cards/swagger-spec.js
+++ documentation/archives/SNAPSHOT/api/cards/swagger-spec.js
@@ -249,8 +249,8 @@ window.swaggerSpec={
     },
     "CardActionEnum" : {
       "type" : "string",
-      "description" : "Defines the action to be executed on card reception >\n* PROPAGATE_READ_ACK_TO_PARENT_CARD\n* KEEP_CHILD_CARDS\n* KEEP_EXISTING_ACKS_AND_READS",
-      "enum" : [ "PROPAGATE_READ_ACK_TO_PARENT_CARD", "KEEP_CHILD_CARDS", "KEEP_EXISTING_ACKS_AND_READS" ],
+      "description" : "Defines the action to be executed on card reception >\n* PROPAGATE_READ_ACK_TO_PARENT_CARD\n* KEEP_CHILD_CARDS\n* KEEP_EXISTING_ACKS_AND_READS,\n* KEEP_EXISTING_PUBLISH_DATE",
+      "enum" : [ "PROPAGATE_READ_ACK_TO_PARENT_CARD", "KEEP_CHILD_CARDS", "KEEP_EXISTING_ACKS_AND_READS", "KEEP_EXISTING_PUBLISH_DATE" ],
       "example" : "PROPAGATE_READ_ACK_TO_PARENT_CARD"
     },
     "Card" : {
