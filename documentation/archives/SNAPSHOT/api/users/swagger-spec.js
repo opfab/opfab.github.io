ec4155ebf66a7f97d4de49927f6bd96188a76fff
OpfabTech <opfabtech@gmail.com>
2025-06-05 00:58:10 +0200

    Updating documentation for version SNAPSHOT

--- documentation/archives/SNAPSHOT/api/users/swagger-spec.js
+++ documentation/archives/SNAPSHOT/api/users/swagger-spec.js
@@ -332,10 +332,6 @@ window.swaggerSpec={
           "type" : "boolean",
           "description" : "If this is set to true, the emails' bodies will be plain text"
         },
-        "disableCardContentInEmails" : {
-          "type" : "boolean",
-          "description" : "If this is set to true, the email templates will be disabled"
-        },
         "sendDailyEmail" : {
           "type" : "boolean",
           "description" : "If this is set to true, an email will be sent daily with all the cards received during the day"
@@ -403,10 +399,6 @@ window.swaggerSpec={
         "emailToPlainText" : {
           "type" : "boolean",
           "description" : "If this is set to true, the emails' bodies will be plain text"
-        },
-        "disableCardContentInEmails" : {
-          "type" : "boolean",
-          "description" : "If this is set to true, the email templates will be disabled"
         },
         "sendDailyEmail" : {
           "type" : "boolean",
